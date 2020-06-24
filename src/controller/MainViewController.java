c479d2b39fcda04a14243b9adf322cf7661cc7f1
delamet <delamet@tamu.edu>
2020-06-24 14:06:36 -0400

    Added other menu items

--- src/controller/MainViewController.java
+++ src/controller/MainViewController.java
@@ -9,4 +9,34 @@ public class MainViewController {
     public void addIncomePressed(Event e){
         System.out.println("Add Income Pressed");
     }
+
+    @FXML
+    public void addReceiptPressed(Event e){
+        System.out.println("Add Receipt Pressed");
+    }
+
+    @FXML
+    public void addCategoryPressed(Event e){
+        System.out.println("Add Category Pressed");
+    }
+
+    @FXML
+    public void deleteIncomePressed(Event e) {
+        System.out.println("Delete Income Pressed");
+    }
+
+    @FXML
+    public void deleteReceiptPressed(Event e) {
+        System.out.println("Delete Receipt Pressed");
+    }
+
+    @FXML
+    public void deleteCategoryPressed(Event e) {
+        System.out.println("Delete Category Pressed");
+    }
+
+    @FXML
+    public void printStatsPressed(Event e) {
+        System.out.println("Print Stats pressed");
+    }
 }
