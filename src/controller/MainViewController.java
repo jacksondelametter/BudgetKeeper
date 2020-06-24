927268804d0fe9bb4235ea64483ef0507fa4d109
delamet <delamet@tamu.edu>
2020-06-24 13:57:34 -0400

    Added event handler to menu item add.

---
+++ src/controller/MainViewController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import javafx.event.Event;
+import javafx.fxml.FXML;
+
+public class MainViewController {
+
+    @FXML
+    public void addIncomePressed(Event e){
+        System.out.println("Add Income Pressed");
+    }
+}
