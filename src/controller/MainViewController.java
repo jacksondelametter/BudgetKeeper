e43ea25ae9fbff5c98f04ca891d167e73d45ebd2
delamet <delamet@tamu.edu>
2020-06-24 17:07:45 -0400

    Added month and year choicebox and populated.

--- src/controller/MainViewController.java
+++ src/controller/MainViewController.java
@@ -1,9 +1,41 @@
 package controller;
 
+import javafx.collections.FXCollections;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class MainViewController {
+
+    @FXML
+    public ChoiceBox monthChoiceBox;
+    @FXML
+    public ChoiceBox yearChoiceBox;
+
+    private void setupMonthYearChoiceBox() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MMM");
+        LocalDateTime rawDate = LocalDateTime.now();
+        String formattedDate = formatter.format(rawDate);
+        String[] splitDate = formattedDate.split("/");
+        String currentMonth = splitDate[1];
+        int currentYear = Integer.parseInt(splitDate[0]);
+        monthChoiceBox.setItems(FXCollections.observableArrayList("Jan", "Feb", "Mar",
+                "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"));
+        monthChoiceBox.setValue(currentMonth);
+        int startYear = 2019;
+        for(int i=startYear;i<=currentYear;i++) {
+            yearChoiceBox.getItems().add(i);
+        }
+        yearChoiceBox.setValue(currentYear);
+    }
+
+    @FXML
+    public void initialize() {
+        setupMonthYearChoiceBox();
+    }
 
     @FXML
     public void addIncomePressed(Event e){
