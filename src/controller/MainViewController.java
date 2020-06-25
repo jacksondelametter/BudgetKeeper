ebf2d14182b01bf179d46f8adae4a93c939831e4
delamet <delamet@tamu.edu>
2020-06-25 14:19:19 -0400

    Added PieChart and TableView

--- src/controller/MainViewController.java
+++ src/controller/MainViewController.java
@@ -3,7 +3,11 @@ package controller;
 import javafx.collections.FXCollections;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -14,6 +18,10 @@ public class MainViewController {
     public ChoiceBox monthChoiceBox;
     @FXML
     public ChoiceBox yearChoiceBox;
+    @FXML
+    public PieChart budgetPieChart;
+    @FXML
+    public TableView budgetTableView;
 
     private void setupMonthYearChoiceBox() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MMM");
@@ -32,9 +40,22 @@ public class MainViewController {
         yearChoiceBox.setValue(currentYear);
     }
 
+    private void setupBudgetPieChart() {
+        PieChart.Data d1 = new PieChart.Data("Utillities", 200);
+        PieChart.Data d2 = new PieChart.Data("Subscriptions", 800);
+
+        budgetPieChart.getData().add(d1);
+        budgetPieChart.getData().add(d2);
+    }
+
+    private void setupBudgetTableView() {
+
+    }
+
     @FXML
     public void initialize() {
         setupMonthYearChoiceBox();
+        setupBudgetPieChart();
     }
 
     @FXML
