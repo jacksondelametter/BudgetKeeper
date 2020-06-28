f5f8af6c50025935974398afad0c350ca996e5c0
delamet <delamet@tamu.edu>
2020-06-28 16:34:51 -0500

    Added categories to services

--- src/service/Database.java
+++ src/service/Database.java
@@ -1,5 +1,6 @@
 package service;
 
+import model.Category;
 import model.Transaction;
 
 import java.sql.*;
@@ -10,35 +11,73 @@ public class Database {
 
     private static String dbUrl = "jdbc:sqlite:/home/jackson/Documents/BudgetKeeper/database/BudgetKeeper.db";
 
+    private static Connection connect() throws Exception {
+        Connection conn = DriverManager.getConnection(dbUrl);
+        return conn;
+    }
+
     static {
-        try (Connection conn = DriverManager.getConnection(dbUrl);
+        try (Connection conn = connect();
              Statement stmt = conn.createStatement()) {
-            String statement = "CREATE TABLE IF NOT EXISTS transactions (date DATE, category STRING, " +
+            String transactionStatement = "CREATE TABLE IF NOT EXISTS transactions (date DATE, category STRING, " +
                     "description STRING, amount DOUBLE);";
-            stmt.execute(statement);
+            String categoryStatement = "CREATE TABLE IF NOT EXISTS categories (name STRING, type STRING);";
+            stmt.execute(transactionStatement);
+            stmt.execute(categoryStatement);
         } catch (Exception e) {
             System.out.println(e.toString());
         }
     }
 
-    public static void addTransaction(Transaction transaction) {
-        try (Connection conn = DriverManager.getConnection(dbUrl);
-             Statement stmt = conn.createStatement()) {
-            String values = transaction.getDate().toString() + "," +
-                    transaction.getType() + "," + transaction.getCategory() + ","
-                    + transaction.getDescription() + "," + transaction.getAmount();
-            String command = "INSERT ONTO transactions (date, category, description, amount)" +
-                    "VALUES (" + values + ");";
-            stmt.execute(command);
+    public static void addCategory(Category category) {
+        String categoryStmt = "INSERT INTO categories (name, type) VALUES (?, ?);";
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(categoryStmt)) {
+            stmt.setString(0, category.getName());
+            stmt.setString(1, category.getType());
+            stmt.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
+    public static ArrayList<Category> getCategories() {
+        ArrayList<Category> categories = new ArrayList<>();
+        try (Connection conn = connect();
+             Statement stmt = conn.createStatement()) {
+            String query = "SELECT * FROM categories;";
+            ResultSet results = stmt.executeQuery(query);
+            while(results.next()) {
+                String name = results.getString(0);
+                String type  = results.getString(1);
+                categories.add(new Category(name, type));
+            }
         } catch (Exception e) {
             System.out.println(e.toString());
+        }
+        return categories;
+    }
+
+    public static void addTransaction(Transaction transaction) {
+        String transactionStmt = "INSERT INTO transactions (date, category, description, amount, type) " +
+                "VALUES (?, ?, ?, ?, ?);";
+        try (Connection conn = connect();
+             PreparedStatement stmt = conn.prepareStatement(transactionStmt)) {
+            stmt.setDate(0, java.sql.Date.valueOf(transaction.getDate().toString()));
+            stmt.setString(1, transaction.getCategory());
+            stmt.setString(2, transaction.getDescription());
+            stmt.setDouble(3, transaction.getAmount());
+            stmt.setString(4, transaction.getType());
+            stmt.executeUpdate();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 
     public static ArrayList<Transaction> getTransactions() {
         ArrayList<Transaction> transactions = new ArrayList<>();
-        try (Connection conn = DriverManager.getConnection(dbUrl);
+        try (Connection conn = connect();
              Statement stmt = conn.createStatement()) {
             String query = "SELECT * FROM transactions;";
             ResultSet results = stmt.executeQuery(query);
