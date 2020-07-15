35bb49d3b56d07568dc0704918bbd86ef8bfd4ce
delamet <delamet@tamu.edu>
2020-07-14 21:48:21 -0500

    Initial commit

---
+++ src/controller/MessageVC.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+import java.awt.*;
+
+public class MessageVC {
+
+    @FXML
+    public Text messageField;
+
+    @FXML
+    public Button okButton;
+
+    @FXML
+    public Button cancelButton;
+}
